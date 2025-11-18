7228ee0c4915c02dfb4f5f826bf5b7525dd39294
efraim urel <efraimurelpalodang@gmail.com>
2025-11-18 11:33:06 +0800

    menambahkan pagination

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -11,37 +11,36 @@ class BarangController extends Controller
 {
     public function index()
     {
-      // Ambil semua data barang dari database
-        $barang = barang::all();
-        
-        // Format data untuk komponen table-row
-        $rows = $barang->map(function ($item) {
+        $barang = Barang::with('satuan')->paginate(7);
+
+        $rows = $barang->getCollection()->map(function ($item) {
             return [
-                'kode' => $item->kode,
-                'nama' => $item->nama_barang,
-                'harga_jual' => 'Rp ' . number_format($item->harga_jual, 0, ',', '.'), // Format harga
-                'stok' => $item->jumlah_stok,
-                'satuan' => $item->satuan->nama,
-                'action' => [
+                'kode'        => $item->kode,
+                'nama'        => $item->nama_barang,
+                'harga_jual'  => 'Rp ' . number_format($item->harga_jual, 0, ',', '.'),
+                'stok'        => $item->jumlah_stok,
+                'satuan'      => $item->satuan?->nama ?? '-',
+                'action'      => [
                     [
-                        'label' => 'Edit',
-                        'url' => '/barang/edit/'.$item->id,
+                        'label'  => 'Edit',
+                        'url'    => '/barang/edit/' . $item->id,
                         'method' => 'GET',
                     ],
                     [
-                        'label' => 'Hapus',
-                        'color' => 'danger',
-                        'url' => '/barang/hapus/'.$item->id,
-                        'method' => 'DELETE',
-                        'confirm' => 'confirmSwal(event, this)',
+                        'label'   => 'Hapus',
+                        'color'   => 'danger',
+                        'url'     => '/barang/hapus/' . $item->id,
+                        'method'  => 'DELETE',
+                        'confirm' => "confirmSwal(event, this, 'Yakin hapus {$item->nama_barang}?')",
                     ],
                 ]
             ];
         })->toArray();
 
-        return view('dashboard.barang.index', [
-            'barang' => $rows
-        ]);
+        // Ganti collection dengan array yang sudah di-map
+        $barang->setCollection(collect($rows));
+
+        return view('dashboard.barang.index', compact('barang'));
     }
 
     public function create()
--- resources/views/dashboard/barang/index.blade.php
+++ resources/views/dashboard/barang/index.blade.php
@@ -1,8 +1,17 @@
 @extends('layouts.app')
 @section('content')
-    <x-table.main title="Barang" :headers="['Kode', 'Nama Barang', 'Harga Jual', 'Stok Barang', 'Satuan', 'Aksi']" routeCreate="/barang/tambah">
+    <x-table.main 
+        title="Barang" 
+        :headers="['Kode', 'Nama Barang', 'Harga Jual', 'Stok Barang', 'Satuan', 'Aksi']" 
+        routeCreate="/barang/tambah"
+    >
         @foreach ($barang as $row)
             <x-table.row :rowData="$row" />
         @endforeach
     </x-table.main>
+    {{-- PAGINATION TETAP MUNCUL! --}}
+    <div class="mt-4 d-flex justify-content-center">
+        {{ $barang->links() }}
+    </div>
+
 @endsection
